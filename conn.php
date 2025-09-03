68394f7fdd63f608d5d6c3dc93153f31d4a55ff1
Isabelle Cruz <bebellecruzx@gmail.com>
2025-09-03 14:48:05 -0300

    Criando as funcionalidades e cadastrando no BD, alem de colocar senha criptografada

--- conn.php
+++ conn.php
@@ -1,4 +1,6 @@
 <?php
+
+
 $servername = "localhost:3306";
 $username = "root";
 $password = "root";
@@ -6,6 +8,17 @@ $dbname = "tasks";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "CREATE TABLE IF NOT EXISTS users (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)";
+if ($conn->query($sql) === FALSE) {
+    echo "Erro ao criar tabela: " . $conn->error;
 }
